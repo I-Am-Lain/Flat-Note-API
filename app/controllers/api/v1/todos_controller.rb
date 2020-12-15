82820feaea33c5b76c90369a941852f35866ccee
I-Am-Lain <vetter.william@gmail.com>
2020-12-15 03:41:59 -0600

    Deliverables Finished

--- app/controllers/api/v1/todos_controller.rb
+++ app/controllers/api/v1/todos_controller.rb
@@ -10,6 +10,15 @@ class Api::V1::TodosController < ApplicationController
         render json: todo
     end
 
+    def update
+        todo = Todo.find(params[:id])
+
+        todo.update(name: params[:name])
+        if todo.valid?
+            render json: todo
+        end
+    end
+
     def destroy
 
         todo = Todo.find(params[:id])
