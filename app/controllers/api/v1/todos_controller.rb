bfc4fd0ec0a6183d37d6973d3867925db9e651e7
I-Am-Lain <vetter.william@gmail.com>
2020-12-15 00:26:20 -0600

    Create+Destroy todos

--- app/controllers/api/v1/todos_controller.rb
+++ app/controllers/api/v1/todos_controller.rb
@@ -5,4 +5,17 @@ class Api::V1::TodosController < ApplicationController
         render json: todos
     end
 
+    def create
+        todo = Todo.create(name: params[:name], user_id: params[:user_id] )
+        render json: todo
+    end
+
+    def destroy
+
+        todo = Todo.find(params[:id])
+        todo.destroy()
+        render json: { success: true }
+
+    end
+
 end
